c82642bc58b8461b13aeaf4fd80eca58d266761d
Nynor <nynor@nfpires.com>
2025-05-05 13:26:32 +0100

    Resolve merge conflicts with remote main

--- src/db.py
+++ src/db.py
@@ -4,12 +4,14 @@ import os
 
 DATA_PATH = "data/expenses.csv"
 
+
 def save_expense(expense):
     df = pd.DataFrame([expense.dict()])
     if os.path.exists(DATA_PATH):
         df.to_csv(DATA_PATH, mode='a', header=False, index=False)
     else:
         df.to_csv(DATA_PATH, index=False)
+
 
 def load_expenses():
     if os.path.exists(DATA_PATH):
